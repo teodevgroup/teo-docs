45a6a7c1bfca6a03d1723133b1f272b4ed499b7b
Victor Teo <victor.teo@fillmula.com>
2024-02-08 04:20:34 +0800

    add frontmatter

--- src/app/api/search/route.ts
+++
@@ -1,9 +0,0 @@
-// import { search } from "@teocloud/teo-docs-search-engine";
-// import { NextResponse, NextRequest } from "next/server";
-
-// // To handle a GET request to /api
-// export async function GET(request: NextRequest) {
-//     const text = request.nextUrl.searchParams['q']
-//     const results = search(text)
-//     return NextResponse.json(results);
-// }
