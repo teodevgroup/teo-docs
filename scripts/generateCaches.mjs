0f20e4242b422206f3766d436bedb188bcbd6bb9
Victor Teo <victor.teo@fillmula.com>
2024-04-01 08:57:05 +0800

    fix typo

--- scripts/generateCaches.mjs
+++ scripts/generateCaches.mjs
@@ -7,7 +7,7 @@ import fixWindowsPath from './fixWindowsPath.mjs'
 
 export default function generateCaches() {
     globSync("./src/app/**/*.mdx").forEach((fileLocation) => {
-        fileLocation = fixWindowsPath(fileLocation);
+        fileLocation = fixWindowsPath(fileLocation)
         generateFullTextIndex(fileLocation)
         generateToc(fileLocation)
         generateBreadcrumb(fileLocation)
