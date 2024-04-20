80e9d5c8412f70272f47d37891d28153987f6d7b
Victor Teo <victor.teo@fillmula.com>
2024-04-20 16:54:17 +0800

    fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default async function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,1000,2000,3000,4000,5000,6000,7000,8001,1001,2001,3001,4001,5001,6001,7001,800&family=Barlow:ital,wght@0,1000,2000,3000,4000,5000,6000,7000,8000,9001,1001,2001,3001,4001,5001,6001,7001,8001,900&family=Inter:wght@100200300400500600700800900&family=JetBrains+Mono:ital,wght@0,1000,2000,3000,4000,5000,6000,7000,8001,1001,2001,3001,4001,5001,6001,7001,800&display=swap" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100,200,300,400,500,600,700,800,900&family=Barlow:ital,wght@0,100,200,300,400,500,600,700,800,900&family=Inter:wght@100,200,300,400,500,600,700,800,900&family=JetBrains+Mono:ital,wght@0,100,200,300,400,500,600,700,800&display=swap" />
       </head>
         
       <Main>
