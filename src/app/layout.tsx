28b487fcd6c32dc1feadd9609da7b497408df3fb
Victor Teo <victor.teo@fillmula.com>
2024-12-08 18:18:35 +0800

    fix font link tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,10 +35,9 @@ export default async function RootLayout({
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100,200,300,400,500,600,700,800,900&family=Barlow:ital,wght@0,100,200,300,400,500,600,700,800,900&family=Inter:wght@100,200,300,400,500,600,700,800,900&family=JetBrains+Mono:ital,wght@0,100,200,300,400,500,600,700,800&display=swap" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet" />
       </head>
-        
       <Main>
         {children}
       </Main>
