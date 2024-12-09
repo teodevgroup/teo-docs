2df3251afb1a3ed9d52e6dd1d8ae1c69e2c693e4
Victor Teo <victor.teo@fillmula.com>
2024-12-09 20:44:36 +0800

    fix sync

--- src/shared/lib/preferences/preferencesClient.ts
+++ src/shared/lib/preferences/preferencesClient.ts
@@ -2,10 +2,31 @@
 
 import useCookie from 'react-use-cookie'
 import Preferences, { PreferencesKey } from './preferences'
+import { useEffect } from 'react'
+
+const callbacks = {
+  'server': [],
+  'database': [],
+  'client': [],
+}
 
 const usePreference = <T extends PreferencesKey, V extends Preferences[T]>(name: T, initialValue: V): [V, (value: V) => void] => {
   const [value, setValue] = useCookie(name, initialValue)
-  return [value as V, setValue]
+  useEffect(() => {
+    callbacks[name].push(setValue)
+    return () => {
+      const index = callbacks[name].indexOf(setValue)
+      callbacks[name].splice(index, 1);
+    }
+  }, [name])
+  const wrappedSetValue = (newValue: string) => {
+    setValue(newValue)
+    for (const key in callbacks[name]) {
+      const setState = callbacks[name][key]
+      setState(newValue)
+    }
+  }
+  return [value as V, wrappedSetValue]
 }
 
 export default usePreference
